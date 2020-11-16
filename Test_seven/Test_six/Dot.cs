71fdc89370b1ae4ac10e255970cc15c64c0aa56f
ImpersonaI <43351355+ImpersonaI@users.noreply.github.com>
2020-11-16 19:27:43 +0300

    Add files via upload

---
+++ Test_seven/Test_six/Dot.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test_six
+{
+    class Dot
+    {
+        double x, y, x1, y1, z, z1;
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public double X1 { get; set; }
+        public double Y1 { get; set; }
+               
+        public Dot ()
+        {
+
+            Console.WriteLine("Input x =");
+            X = Convert.ToDouble(Console.ReadLine());
+            this.x = X;
+            Console.WriteLine("Input y =");
+            Y = Convert.ToDouble(Console.ReadLine());
+            this.y = Y;
+        }
+
+        public Dot(double x, double y)
+        {
+
+            z = Math.Sqrt((x - 0) * (x - 0) + (y - 0) * (y - 0));
+            Console.WriteLine("Расстояние до начала координат = " + z);
+            Console.WriteLine("Input x1 =");
+            X1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Input y1 =");
+            Y1 = Convert.ToDouble(Console.ReadLine());
+
+        }
+
+        public void Do(double x1, double y1)
+        {
+
+            z1 = Math.Sqrt((X1 - X)* (X1 - X) + (Y1 - Y)* (Y1 - Y));
+
+        }
+
+        public void Do()
+        {
+
+            Console.WriteLine("Расстояние от точки (X1;Y1) до точки (X;Y) = " + z1);
+        }
+
+       
+        
+    }
+}
---
+++ Test_seven/Test_six/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Dynamic;
+using System.IO;
+
+namespace Test_six
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"log_output.txt");
+            var new_in = new StreamReader(@"log_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Dot dot = new Dot();
+            Dot dot2 = new Dot(dot.X, dot.Y);
+            dot2.Do(dot2.X1, dot2.Y1);
+            dot2.Do();
+#if !DEBUG
+            Console.SetOut(save_out); new_out.Close();
+            Console.SetIn(save_in); new_in.Close();
+#else
+            Console.ReadKey();
+#endif
+
+        }
+    }
+}
